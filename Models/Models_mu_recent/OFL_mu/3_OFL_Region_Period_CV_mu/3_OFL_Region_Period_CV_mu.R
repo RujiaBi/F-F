1d09ec746f8cf4a7bdcc4d8bef0956415584994e
RujiaBi <rbi@vt.edu>
2024-09-04 14:50:59 -0700

    Models_mu_base

---
+++ Models/Models_mu_recent/OFL_mu/3_OFL_Region_Period_CV_mu/3_OFL_Region_Period_CV_mu.R
@@ -0,0 +1,95 @@
+# Clear workspace
+rm(list = ls())
+
+# Turn off scientific notation
+options(scipen=999)
+
+# Packages
+source('util.R')
+
+#####################################################################################################
+
+# Define directories
+dat <- read.csv("new_data_council_model_revision.csv", as.is=T)
+dat0 <- dat[!is.na(dat$OFL_ty_recent) & !is.na(dat$OFL_ty_old),]
+dat0[dat0$num_assess==2,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==2,]$stockid))
+dat0[dat0$num_assess==3,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==3,]$stockid))
+dat0[dat0$num_assess==4,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==4,]$stockid))
+dat0[dat0$num_assess==5,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==5,]$stockid))
+dat0[dat0$num_assess==6,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==6,]$stockid))
+dat0[dat0$num_assess==7,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==7,]$stockid))
+dat0[dat0$num_assess==8,]$stockid_code <- as.numeric(as.factor(dat0[dat0$num_assess==8,]$stockid))
+dd <- dat0[,c(3,28,8,9,11:26,30:36)]
+
+data_2ass <- dd[dd$num_assess==2,]
+data_3ass <- dd[dd$num_assess==3,]
+data_4ass <- dd[dd$num_assess==4,]
+data_5ass <- dd[dd$num_assess==5,]
+data_6ass <- dd[dd$num_assess==6,]
+data_7ass <- dd[dd$num_assess==7,]
+data_8ass <- dd[dd$num_assess==8,]
+
+## Stock number
+stocknum_2ass <- length(unique(data_2ass$stockid_code))
+stocknum_3ass <- length(unique(data_3ass$stockid_code))
+stocknum_4ass <- length(unique(data_4ass$stockid_code))
+stocknum_5ass <- length(unique(data_5ass$stockid_code))
+stocknum_6ass <- length(unique(data_6ass$stockid_code))
+stocknum_7ass <- length(unique(data_7ass$stockid_code))
+stocknum_8ass <- length(unique(data_8ass$stockid_code))
+
+#################################################################################################################
+
+data = list(data_2ass = as.matrix(data_2ass),
+            data_3ass = as.matrix(data_3ass),
+            data_4ass = as.matrix(data_4ass),
+            data_5ass = as.matrix(data_5ass),
+            data_6ass = as.matrix(data_6ass),
+            data_7ass = as.matrix(data_7ass),
+            data_8ass = as.matrix(data_8ass),
+            
+            # Stock number
+            stocknum_2ass = as.numeric(stocknum_2ass),
+            stocknum_3ass = as.numeric(stocknum_3ass),
+            stocknum_4ass = as.numeric(stocknum_4ass),
+            stocknum_5ass = as.numeric(stocknum_5ass),
+            stocknum_6ass = as.numeric(stocknum_6ass),
+            stocknum_7ass = as.numeric(stocknum_7ass),
+            stocknum_8ass = as.numeric(stocknum_8ass),
+            
+            # Stock_compare_num
+            stock_compare_num_3ass = as.numeric(dim(data_3ass)[1]),
+            stock_compare_num_4ass = as.numeric(dim(data_4ass)[1]),
+            stock_compare_num_5ass = as.numeric(dim(data_5ass)[1]),
+            stock_compare_num_6ass = as.numeric(dim(data_6ass)[1]),
+            stock_compare_num_7ass = as.numeric(dim(data_7ass)[1]),
+            stock_compare_num_8ass = as.numeric(dim(data_8ass)[1]),
+            
+            # ty number (for stocks with 2+ assesses)
+            tynum_3ass = 2,
+            tynum_4ass = 3,
+            tynum_5ass = 4,
+            tynum_6ass = 5,
+            tynum_7ass = 6,
+            tynum_8ass = 7,
+            
+            # number of regions
+            rs = 20)
+
+
+# Parameters monitored
+params = c("cv_global", "cv_region", "cv", "nu_r", "nu_tp", 
+           "loglik_2ass", "loglik_3ass", "loglik_4ass", "loglik_5ass", "loglik_6ass", "loglik_7ass", "loglik_8ass") 
+
+# Run jags model
+system.time(fit <- run.jags(model="3_OFL_Region_Period_CV_mu.txt", monitor=params, data=data, burnin=10000, sample=1000, adapt=10000, thin=20, n.chains=5, method = 'rjparallel'))
+save(fit, file = '3_OFL_Region_Period_CV_mu_fit.RData')
+dic=extract(fit, 'dic')
+save(dic, file='3_OFL_Region_Period_CV_mu_dic.RData')
+
+sum(dic$deviance)+sum(dic$penalty)
+
+mcmc <- as.mcmc(fit)
+pdf("traceplot.pdf") 
+traceplot(mcmc)
+dev.off()
---
+++ Models/Models_mu_recent/OFL_mu/4_OFL_Region_CV_constant_mu/Buffer_OFLCV.R
@@ -0,0 +1,89 @@
+# qlnorm, inverse c. d. f.
+# Pick four means of log(OFL) as examples: 0, 5, 10, 15
+
+# 1st
+cvlog <- seq(0,2.5,by=0.01)
+sdlog <- sqrt(log(cvlog^2+1))
+meanlog <- 0
+abc1_0.4 <- qlnorm(p=0.4, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+abc1_0.45 <- qlnorm(p=0.45, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+buf1_0.4 <- (exp(meanlog)-abc1_0.4)/exp(meanlog)
+buf1_0.45 <- (exp(meanlog)-abc1_0.45)/exp(meanlog)
+
+# 2nd
+cvlog <- seq(0,2.5,by=0.01)
+sdlog <- sqrt(log(cvlog^2+1))
+meanlog <- 5
+abc2_0.4 <- qlnorm(p=0.4, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+abc2_0.45 <- qlnorm(p=0.45, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+buf2_0.4 <- (exp(meanlog)-abc2_0.4)/exp(meanlog)
+buf2_0.45 <- (exp(meanlog)-abc2_0.45)/exp(meanlog)
+
+# 3rd
+cvlog <- seq(0,2.5,by=0.01)
+sdlog <- sqrt(log(cvlog^2+1))
+meanlog <- 10
+abc3_0.4 <- qlnorm(p=0.4, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+abc3_0.45 <- qlnorm(p=0.45, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+buf3_0.4 <- (exp(meanlog)-abc3_0.4)/exp(meanlog)
+buf3_0.45 <- (exp(meanlog)-abc3_0.45)/exp(meanlog)
+
+# 4th
+cvlog <- seq(0,2.5,by=0.01)
+sdlog <- sqrt(log(cvlog^2+1))
+meanlog <- 15
+abc4_0.4 <- qlnorm(p=0.4, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+abc4_0.45 <- qlnorm(p=0.45, meanlog=meanlog, sdlog=sdlog, lower.tail = TRUE, log.p = FALSE)
+buf4_0.4 <- (exp(meanlog)-abc4_0.4)/exp(meanlog)
+buf4_0.45 <- (exp(meanlog)-abc4_0.45)/exp(meanlog)
+
+# combine results
+res <- data.frame(matrix(NA, nrow=length(cvlog)*4*2, ncol=4))
+colnames(res) <- c("cvlog", "group", "p", "buf")
+res$cvlog <- rep(cvlog*100, times=4*2)
+res$group <- rep(1:4, each=length(cvlog)*2)
+res$p <- rep(c(0.4,0.45,0.4,0.45,0.4,0.45,0.4,0.45), each=length(cvlog))
+res$buf  <- c(buf1_0.4, buf1_0.45, buf2_0.4, buf2_0.45, buf3_0.4, buf3_0.45, buf4_0.4, buf4_0.45)
+res$buf <- res$buf*100
+res$group <- as.factor(res$group)
+res$p <- as.factor(res$p)
+
+# plot
+library(ggplot2)
+library(ggpubr)
+library(cowplot)
+library(grid)
+library(Ipaper)
+cbPalette2 <- c("#0072B2", "#CC79A7")
+
+# After plotting, mean value doesn't matter.
+p <- ggplot(data=res[res$group==4,]) +
+  geom_line(aes(x=cvlog, y=buf, color=p), size=1.3) +
+  scale_colour_manual(name="P*", values=cbPalette2) +
+  ylab("Difference between ABC and OFL (%)") +
+  xlab("OFL CV (%)") +
+  theme_bw() + 
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.text.y   = element_text(size=20),
+        axis.text.x   = element_text(size=20, vjust = 0.5, hjust=1),
+        axis.title.y  = element_text(size=22),
+        axis.title.x  = element_text(size=22),
+        legend.title=element_text(size=22),
+        legend.text=element_text(size=20),
+        legend.justification=c(1,1),
+        legend.position=c(0.98,0.1),
+        legend.direction = "horizontal",
+        strip.text.x = element_text(size=22),
+        strip.text.y = element_text(size=22),
+        plot.margin = unit(c(0.5,2,0.5,0.5), "lines"),
+        panel.spacing = unit(1, "lines")) +
+  guides(fill = guide_legend(nrow = 1, byrow = T))
+
+png("Buffer_CV.png", height = 6.5, width = 8, units='in', res=600)
+p
+dev.off()
+
+
